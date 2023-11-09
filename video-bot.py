b16b26594144eb758c4226723169c2eddcef2004
carsca <carlolscarpa@gmail.com>
2023-11-08 17:51:59 -0700

    milestone reached

--- video-bot.py
+++ video-bot.py
@@ -6,7 +6,6 @@ from moviepy.editor import *
 import random
 
 
-# Left off trying to solve why text only appears above y = 50
 
 class Trivia:
     def __init__(self, question, options, answer):
@@ -21,6 +20,24 @@ class Trivia:
         self.question = question
         self.options = options
         self.answer = answer
+
+    def get_question(self):
+        '''
+        Purpose of this getter is to avoid questions from going off the edge of the screen
+        
+        '''
+        MAX_CHARACTERS = 40
+        # Check if the text exceeds the maximum character count
+        if len(self.question) > MAX_CHARACTERS:
+            # Find the position of the last space before the maximum character count
+            last_space = self.question.rfind(' ', 0, MAX_CHARACTERS)
+            if last_space != -1:  # If a space is found within the limit
+                question_text = self.question[:last_space] + '\n' + self.question[last_space + 1:]
+            else:  # If no space found within the limit, insert newline directly
+                question_text = self.question[:MAX_CHARACTERS] + '\n' + self.question[MAX_CHARACTERS:]
+        else:
+            question_text = self.question
+        return question_text
 
     def __repr__(self):
         return f"text output: {self.question} {', '.join(self.options)} {self.answer}"
@@ -39,27 +56,33 @@ class TriviaVideoGenerator:
 
 
     def generate_trivia_video(self, Trivia):
-        # create clips array
+        
+        # Create clips array
         clips = []
-        # add a TextClip of the question to the array
-        clips.append(TextClip(Trivia.question, fontsize=50, color='white').set_position((100, 100)).set_start(0).set_duration(5))
-
+        clips.append(TextClip(Trivia.get_question(), color='white', stroke_color='black', stroke_width=2, fontsize=70, font="Bernard-MT-Condensed", size=(1080,1920), align="center").set_position((0,-700)).set_duration(1).set_start(0))
 
         # Loop through all options, adjusting their positions accordingly
-        ypos = 960
+        ypos = -500
         for op in Trivia.options:
-            clips.append(TextClip(op, fontsize=40, color='white').set_position((540, 40)).set_start(0).set_duration(5))
-            ypos -= 20
-        # add the answer to the array
-        clips.append(TextClip(Trivia.answer, fontsize=30, color='black').set_position((0, 960)).set_start(0).set_duration(5))
-        
-        # convert array into all the clips concatenated
-        final_text_clip = CompositeVideoClip(clips)
-        # get background
-        background = VideoFileClip(self.background).subclip(0, 5)
-        # merge these clips together 
-        final_clip = CompositeVideoClip([background, final_text_clip])
+            clips.append(TextClip(op, color='white', stroke_color='black', stroke_width=2, fontsize=100, font="Bernard-MT-Condensed", size=(1080,1920), align="center").set_position((0,ypos)).set_duration(1).set_start(0))
+            ypos += 250
 
+        # add the answer to the array
+        answer_text = TextClip(Trivia.answer, color='white', stroke_color='black', stroke_width=3, fontsize=200, font="Bernard-MT-Condensed", size=(1080,1920), align="center").set_position((0,0)).set_duration(1).set_start(0)
+
+
+        # The backgrround clip
+        background = VideoFileClip(self.background).subclip(0, 1)
+        # The initial text clips
+        final_text_clip = CompositeVideoClip(clips)
+        # Initial text on top of background
+        final_clip = CompositeVideoClip([background, final_text_clip])
+        
+        # Revealed answer clip
+        answer_clip = CompositeVideoClip([background, answer_text])
+        
+        # The actual final video
+        final_clip = concatenate_videoclips([final_clip, answer_clip])
         return final_clip
 
 def create_questions():
@@ -97,8 +120,10 @@ def main():
 
     curr_question = questions[0]
 
+
     video = video_generator.generate_trivia_video(curr_question)
     video.write_videofile("new_video.mp4")
+    
 
 if __name__ == "__main__":
     main()
